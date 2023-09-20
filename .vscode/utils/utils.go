14f31409c6c620dd8d051d078efe7adb43047d46
Marc Pontida <pontidmc@skysoft-atm.com>
2023-09-20 15:12:45 +0200

    add utils

---
+++ .vscode/utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+func ReadInputLines(filename string) []string {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		Err("could not read file '%s'", err, filename)
+	}
+	allLines := string(bytes)
+	linesRaw := strings.Split(allLines, "\n")
+	lines := make([]string, 0)
+	for _, line := range linesRaw {
+		l := strings.Trim(line, " ")
+		if len(l) > 0 {
+			lines = append(lines, l)
+		}
+	}
+	return lines
+}

f8378b62769171c93e1fcf55a831b71b6300b729
rondoudou <marcpon@yahoo.fr>
2023-11-15 18:40:17 +0100

    day4

--- day04/04.go
+++ day04/04.go
@@ -9,37 +9,33 @@ import (
 )
 
 func main() {
-	fmt.Println(step1("04_test.txt"))
-	fmt.Println(step1("04.txt"))
-	//fmt.Println(step2("04_test.txt"))
-	//fmt.Println(step2("04.txt"))
+	fmt.Println(step1and2("04_test.txt"))
+	fmt.Println(step1and2("04.txt"))
 }
 
-func step1(filename string) int {
+func step1and2(filename string) (int, uint32) {
 	result := 0
-
+	var result2 uint32 = 0
 	lines := aoc.ReadInputLines(filename)
 	for _, line := range lines {
 		strs := strings.Split(line, ",")
 		set1 := getAllInt(strs[0])
 		set2 := getAllInt(strs[1])
-		set3 := set1.Union(set2)
-		if set3.Size() == set2.Size() && (set3.Size() == set1.Size() || set3.Size() == set2.Size()) {
+		set3 := set1.Intersection(set2)
+		if set3.Size() > 0 && (set3.Size() == set1.Size() || set3.Size() == set2.Size()) {
 			result++
 		}
+		if set3.Size() > 0 {
+			result2++
+		}
 	}
-	return result
-}
-
-func step2(filename string) int {
-	result := 0
-
-	return result
+	return result, result2
 }
 
 func getAllInt(str string) *aoc.Set[int] {
 	result := aoc.NewSet[int]()
 	strs := strings.Split(str, "-")
+
 	if strs[0] == "" || strs[1] == "" {
 		return result
 	}
@@ -47,11 +43,10 @@ func getAllInt(str string) *aoc.Set[int] {
 	index2, err2 := strconv.Atoi(strs[1])
 
 	if err1 == nil && err2 == nil {
-		for i := index1; i < index2; i++ {
+		for i := index1; i <= index2; i++ {
 			result.Add(i)
 		}
-	} else {
-		fmt.Println("oups")
 	}
+
 	return result
 }

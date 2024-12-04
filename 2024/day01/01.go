156a6fb980258c87cee256fc94d66c587c071aa9
rondoudou <marcpon@yahoo.fr>
2024-12-04 20:54:58 +0100

    star2

--- 2024/day01/01.go
+++ 2024/day01/01.go
@@ -11,11 +11,13 @@ import (
 )
 
 func main() {
-	aoc.CheckEquals("11", step1("01_test1.txt"))
-	fmt.Println(step1("01.txt"))
+	aoc.CheckEquals("11", step1("01_test1.txt", true))
+	fmt.Println(step1("01.txt", true))
+	aoc.CheckEquals("31", step1("01_test2.txt", false))
+	fmt.Println(step1("01.txt", false))
 }
 
-func step1(filename string) string {
+func step1(filename string, isStep1 bool) string {
 	lines := aoc.ReadInputLines(filename)
 
 	col1 := make([]int, 0)
@@ -23,6 +25,7 @@ func step1(filename string) string {
 	var val1 int
 	var val2 int
 	var err error
+	col2Cnt := make(map[int]int, 0)
 	for _, val := range lines {
 		splits := strings.Split(val, "   ")
 		if len(splits) != 2 {
@@ -41,6 +44,7 @@ func step1(filename string) string {
 		}
 		col1 = append(col1, val1)
 		col2 = append(col2, val2)
+		col2Cnt[val2]++
 	}
 	sort.Ints(col1)
 	sort.Ints(col2)
@@ -48,10 +52,20 @@ func step1(filename string) string {
 	if len(col1) != len(col2) {
 		return "diff in length"
 	}
-	var dist float64
-	for i := range col1 {
-		dist += math.Abs(float64(col1[i] - col2[i]))
+	if isStep1 {
+		var dist float64
+		for i := range col1 {
+			dist += math.Abs(float64(col1[i] - col2[i]))
+		}
+		return fmt.Sprintf("%d", int(dist))
+	} else {
+		var result int
+		for _, val := range col1 {
+			cnt, exist := col2Cnt[val]
+			if exist {
+				result += cnt * val
+			}
+		}
+		return fmt.Sprintf("%d", result)
 	}
-
-	return fmt.Sprintf("%f", dist)
 }

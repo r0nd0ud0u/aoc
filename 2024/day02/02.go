3e7c32134c4d94ad55f58581d8ebdf7ec434c79e
rondoudou <marcpon@yahoo.fr>
2024-12-05 14:00:00 +0100

    star2

--- 2024/day02/02.go
+++ 2024/day02/02.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -12,7 +13,9 @@ import (
 func main() {
 	aoc.CheckEquals("2", step1("test1.txt"))
 	fmt.Println(step1("input.txt"))
-	aoc.CheckEquals("0", step1("test2.txt"))
+	aoc.CheckEquals("4", step2("test2.txt"))
+	aoc.CheckEquals("1", step2("test3.txt"))
+	fmt.Println(step2("input.txt"))
 }
 
 func step1(filename string) string {
@@ -68,4 +71,59 @@ func step1(filename string) string {
 		}
 	}
 	return fmt.Sprintf("%d", safe)
+}
+
+type lvls struct {
+	line []int
+	cnt  int
+}
+
+func step2(filename string) string {
+	lines := aoc.ReadInputLines(filename)
+	newLines := make([][]int, 0)
+	for _, val := range lines {
+		splits := strings.Split(val, " ")
+		newArr := make([]int, 0)
+		for _, val := range splits {
+			lvl, err := strconv.ParseInt(val, 10, 32)
+			if err != nil {
+				return "error conv lvl"
+			}
+			newArr = append(newArr, int(lvl))
+		}
+		newLines = append(newLines, newArr)
+	}
+	var cnt int
+	for _, line := range newLines {
+		if isSafe(line) {
+			cnt++
+			continue
+		}
+
+		for i := 0; i < len(line); i++ {
+			tmp := slices.Clone(line)
+			// check lvl by lvl
+			tmp = append(tmp[:i], tmp[i+1:]...)
+			if isSafe(tmp) {
+				cnt++
+				break
+			}
+		}
+	}
+	return fmt.Sprintf("%d", cnt)
+}
+
+func isSafe(line []int) bool {
+	isAscending := line[1] > line[0]
+	for i := 1; i < len(line); i++ {
+		diff := math.Abs(float64(line[i]) - float64(line[i-1]))
+		if diff < 1 || diff > 3 {
+			return false
+		}
+		if (line[i] > line[i-1] && isAscending) || (line[i] < line[i-1] && !isAscending) {
+			continue
+		}
+		return false
+	}
+	return true
 }

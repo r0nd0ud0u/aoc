ffd39d8ce2c601997bb75c6b2bd5f3d6b8389b8b
rondoudou <marcpon@yahoo.fr>
2025-12-16 00:23:26 +0100

    update gitignore

--- 2025/day01/src/star2.rs
+++ 2025/day01/src/star2.rs
@@ -1,9 +1,9 @@
+use anyhow::Result;
 use std::path::Path;
 
-use anyhow::Result;
-
-pub fn star1<P: AsRef<Path>>(path: P) -> Result<i32> {
+pub fn star2<P: AsRef<Path>>(path: P) -> Result<i32> {
     let lines = rust_utils::utils::read_lines(path)?;
+
     let (_, output) = lines.iter().fold((50, 0), |(mut dial, mut output), item| {
         let mut chars = item.chars();
 
@@ -13,18 +13,21 @@ pub fn star1<P: AsRef<Path>>(path: P) -> Result<i32> {
             _ => return (dial, output),
         };
 
-        let value: i32 = match chars.as_str().parse() {
+        let steps: i32 = match chars.as_str().parse() {
             Ok(v) => v,
             Err(_) => return (dial, output),
         };
 
-        dial = dial + sign * value;
-        dial = ((dial % 100) + 100) % 100;
-        if dial == 0 {
-            output = output + 1
-        };
+        // simulate each step of the rotation
+        for _ in 0..steps {
+            dial = (dial + sign + 100) % 100;
+            if dial == 0 {
+                output += 1;
+            }
+        }
 
         (dial, output)
     });
+
     Ok(output)
 }
